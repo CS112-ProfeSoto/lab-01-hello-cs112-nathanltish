0f8025177e4c36a7dff0b6989236e6300e0029be
nathanltish <149440288+nathanltish@users.noreply.github.com>
2025-06-13 05:11:55 +0000

    Part 1 commit

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+    // Passes a name argument and gives a greeting alongside desired grade from 2nd method
+    public static void greeting(String name) {
+        System.out.println("Hello " + name + "! Glad to have you in CS112!");
+        System.out.println("Make sure to secure that " + desiredGrade('A') + " you want in this class!");
+    }
+
+    // Very mundane method that returns the parameter passed to it 
+    public static char desiredGrade(char letter) {
+        return letter;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS112!");
+
+        greeting("Nathan");
+        desiredGrade('a');
     }
 }
