72a60ebb642cb64cb2aec1d7b9317ab421651b26
nathanltish <149440288+nathanltish@users.noreply.github.com>
2025-06-13 08:00:51 +0000

    Part 2 commit

--- Main.java
+++ Main.java
@@ -2,11 +2,29 @@
  * CS112 - Lab 01
  * 
  *  @author Nathan Tshishimbi <W7404862@student.miracosta.edu>
- *  @version 1.0 (06/10/25)
+ *  @version 1.0 (06/12/25)
  */
 public class Main {
-    
+
     public static void main(String[] args) {
         System.out.println("Welcome to CS112!");
+
+        System.out.println("Do I like reading?\n" + Main.checksHobby("reading"));
+        
+        Main.printHobbies();
+    }
+
+    public static void printHobbies() {
+        System.out.println("Some of my hobbies include the gym, basketball, running, reading books and manga, pottery, and watching anime, movies, and shows.");
+    }
+
+    public static String checksHobby(String hobby) {
+        String myHobbies = "Some of my hobbies include the gym, basketball, running, reading books and manga, pottery, and watching anime, movies, and shows.";
+
+        if (myHobbies.contains(hobby)) {
+            return "Yes! You like " + hobby + "!";
+        } else {
+            return "No unfortunately you don't :(";
+        }
     }
 }
